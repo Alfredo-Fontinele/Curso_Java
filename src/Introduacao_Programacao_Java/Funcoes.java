cf1ef8d0fd530568039e8d7c3fcd5c27eaa9610f
Alfredo Neto <alfredofontinele08@gmail.com>
2022-04-21 15:50:30 -0300

    First Commit

---
+++ src/Introduacao_Programacao_Java/Funcoes.java
@@ -0,0 +1,14 @@
+package Introduacao_Programacao_Java;
+
+public class Funcoes {
+
+    public static void main(String[] args) {
+        Sum(10, 20);
+    }
+
+    static void Sum(int num1, int num2) {
+        var resultado = num1 + num2;
+        System.out.println("\nResultado da Soma: " + resultado);
+    }
+
+}
---
+++ src/Introduacao_Programacao_Java/MeuPrimeiroPrograma.java
@@ -0,0 +1,9 @@
+package Introduacao_Programacao_Java;
+
+public class MeuPrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("\nHello World. This is my first program with Java\n");
+    }
+
+}
