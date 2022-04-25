5668defb9ee5112c36e8da934f5516a237b45b22
Alfredo Neto <alfredofontinele08@gmail.com>
2022-04-25 20:46:06 -0300

    Fifteenth Commit

--- src/Curso_Java/Arrays/Metodos_ArrayList/Metodos.java
+++ src/Curso_Java/Arrays/Metodos_ArrayList/Metodos.java
@@ -1,6 +1,5 @@
 package Curso_Java.Arrays.Metodos_ArrayList;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,18 +23,39 @@ public class Metodos {
         Names.add("Lucas");
         Names.add("Mateus");
 
-
-
         // 2º) Como buscar elementos pelo índice no Array?
 
-//        for (int i = 0; i < Names.size(); i++) {
-//            System.out.println(Names.get(i));
-//        }
+        for (int i = 0; i < Names.size(); i++) {
+            System.out.println(Names.get(i));
+        }
 
         // 3º) Como identificar um elemento pelo indice?
 
-//        int index = Names.indexOf("Alfredo");
-//        System.out.format("\nElemento: %s\nIndice do Elemento: %s\n", Names.get(index), index);
+        int index = Names.indexOf("Alfredo");
+        System.out.format("\nElemento: %s\nIndice do Elemento: %s\n", Names.get(index), index);
+
+        // 4º) Verificando se a lista está vazia
+
+        System.out.println(Names.isEmpty());
+
+        // 5º) Verificando se a lista contem um determinado elemento
+
+        System.out.println(Names.contains("Alfredo"));
+
+        // 6º) Verificando o tamanho da lista
+
+        System.out.println(Names.size());
+
+        // 7º) Limpando a lista de 2 formas
+
+        // 1º forma
+
+         Names = null;
+
+        // 2º forma
+
+        Names.clear();
+        Names.isEmpty(); //verificando se a lista está vazia
 
     }
 
